3d5a2cd6ef6bf8fa44511c025d638529eaddb5d9
Yu Xing <frank.xingyu@gmail.com>
2021-05-11 17:17:26 -0400

    code without test

--- train.py
+++ train.py
@@ -1,28 +1,44 @@
 import argparse
+
+import ocnn
 import torch
 from torch.backends import cudnn
+
+from utils import load_modelnet40
+import numpy as np
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 if not torch.cuda.is_available():
     raise Exception("We have to use cuda in this project")
 
-
 def main(args):
 
     # create checkpoint folder
 
     # load data (we want 70% data for training 15% data for validation 15% of data for testing)
+    train_points, train_labels, train_categories = load_modelnet40(train=True)
+    num_of_train_data = len(train_points)
+
 
     # get our model
-    model = torch.nn.Module # change this line
+    model = ocnn.LeNet(args.depth, args.channel, args.nout)
     model.to(device)
+
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=(120, 180, 240), gamma=0.1)
+
 
     # cudnn will optimize execution for our network
     cudnn.benchmark = True             # in hw4 our TA has this line of code
 
     if args.evaluate:
         print("\nEvaluation only")
+
+        test_points, test_labels, test_categories = load_modelnet40(train=False)
+        num_of_test_data = len(test_points)
 
         # load the best check point
 
@@ -31,21 +47,42 @@ def main(args):
         return
 
     # get data ready?
-
+    n_points_train = int(args.train_split_ratio * num_of_train_data)
+    full_indices = np.arange(num_of_train_data)
+    np.random.shuffle(full_indices)
+    train_indices = full_indices[:n_points_train]
+    val_indices = full_indices[n_points_train:]
     # perform training
 
     # we have this line in hw4
     # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, args.schedule, gamma=args.gamma)
 
-    # for epoch in range(args.start_epoch, args.epochs):
+    for epoch in range(args.start_epoch, args.epochs):
 
         # train loss
+        train_loss = train()
         # validation loss
+        val_loss = val()
         # scheduler step
+        scheduler.step()
         # save check point
         # print something to the console
+        print("[train loss] " + str(train_loss))
+        print("[val loss] " + str(val_loss))
+
+def train(model, dataset, optimizer, args):
+    model.train()
 
 
 
 if __name__ == "__main__":
+
+
+
     parser = argparse.ArgumentParser(description='Octree classification')
+
+    args = parser.parse_args()
+
+
+
+    main(args)
--- utils.py
+++ utils.py
@@ -1,4 +1,70 @@
+import os
 import torch
+import ocnn
+from tqdm import tqdm
+
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def load_modelnet40(train=True, suffix='points'):
+    root = 'dataset/ModelNet40.points'
+    points, labels = [], []
+    folders = sorted(os.listdir(root))
+    assert len(folders) == 40
+    for idx, folder in enumerate(folders):
+        subfolder = 'train' if train else 'test'
+        current_folder = os.path.join(root, folder, subfolder)
+        filenames = sorted(os.listdir(current_folder))
+        for filename in filenames:
+            if filename.endswith(suffix):
+                filename_abs = os.path.join(current_folder, filename)
+                points.append(np.fromfile(filename_abs, dtype=np.uint8))
+                labels.append(idx)
+    return points, labels, folders
+
+
+class ourDataset:
+
+    def __init__(self, points, labels, phase="train", args=None):
+        self.phase = phase
+        self.points = points
+        self.labels = labels
+        self.number_samples = len(points)
+
+        if self.phase == "train":
+            self.bs = args.train_batch
+
+    def get_batch(self, idx):
+        start_idx = idx * self.bs
+        end_idx = min(start_idx + self.bs, self.number_samples)  # exclusive
+        if self.phase == 'val':
+            curr_points = self.points[start_idx:end_idx, :]
+            curr_labels = self.labels[start_idx:end_idx]
+        if self.phase == "train":
+            random_index = np.random.choice(len(self.points), self.bs)
+            curr_points = self.points[random_index]
+            curr_labels = self.labels[random_index]
+        flags = vars(self.args)
+        transform = ocnn.TransformCompose(flags)
+        batch = []
+        # for each curr_points
+        for index, point in enumerate(curr_points):
+            # 1. to tensor
+            t_point = point.to(device)
+            # 2. call transform
+            octree = transform(t_point)
+            # 3. form a tuple with corresponding label and add to a new list
+            batch.append((octree, curr_labels[index]))
+
+            # ocnn.collate_octrees with the new list(batch)
+            # the result from it should be super_octree and curr_label
+            # return both
+            return ocnn.collate_octrees(batch)
 
 
 
+if __name__ == '__main__':
+    load_modelnet40()
